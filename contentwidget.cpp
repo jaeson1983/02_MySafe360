085cae819387d0febc618047b2f0a4180e482590
Jaeson1983 <77425893+jaeson1983@users.noreply.github.com>
2024-05-20 15:57:35 +0800

    Add files via upload

---
+++ contentwidget.cpp
@@ -0,0 +1,10 @@
+﻿#include "contentwidget.h"
+
+ContentWidget::ContentWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QPalette palette;
+    palette.setBrush(QPalette::Window, QBrush(Qt::white));
+    setPalette(palette);
+    setAutoFillBackground(true);
+}
---
+++ contentwidget.h
@@ -0,0 +1,17 @@
+#ifndef CONTENTWIDGET_H
+#define CONTENTWIDGET_H
+
+#include <QWidget>
+
+class ContentWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ContentWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // CONTENTWIDGET_H
